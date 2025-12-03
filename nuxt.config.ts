1ded03df33510c75cd95026676603cee10be0815
danik-progger <lyatorovskiy@yandex.ru>
2025-12-03 23:27:32 +0300

    Added tailwind, daisyui and icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  modules: ["@nuxt/eslint"],
+  modules: ["@nuxt/eslint", "nuxt-icons"],
+  css: ["~/assets/css/main.css"],
   eslint: {
     config: {
       standalone: false,
     },
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
